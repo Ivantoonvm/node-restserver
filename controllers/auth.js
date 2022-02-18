45e20bf75ed6d621489cb8006846005650500f75
Ivantoonvm <ivantoonvm0@gmail.com>
2022-02-17 20:49:50 -0600

    Google sing added

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,8 @@ const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario');
 
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleverify } = require('../helpers/google-verify');
+const usuario = require('../models/usuario');
 
 
 const login = async(req, res = response) => {
@@ -52,8 +54,58 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSingIn = async (req,res=response)=>{
+   
+    const {id_token} = req.body;
+
+
+    try {
+        
+        const {nombre,img,correo}= await googleverify(id_token); 
+        
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            //Tengo que crearlo 
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol: 'USER_ROLE',
+                google : true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si el usuario en DB 
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'EL token no se pudo verificar'
+        })
+    }
+    
+}
 
 
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
